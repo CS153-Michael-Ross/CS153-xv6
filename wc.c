6ade2921493ec5109f98ea25eae4f5ad0fb931a3
Ross McGuyer <rmcgu002@sledge.cs.ucr.edu>
2018-10-10 19:55:12 -0700

    stashed changes

--- wc.c
+++ wc.c
@@ -1,7 +1,7 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-
+#include "stdlib.h"
 char buf[512];
 
 void
@@ -27,7 +27,7 @@ wc(int fd, char *name)
   }
   if(n < 0){
     printf(1, "wc: read error\n");
-    exit();
+    exit(EXIT_FAILURE);
   }
   printf(1, "%d %d %d %s\n", l, w, c, name);
 }
@@ -39,16 +39,16 @@ main(int argc, char *argv[])
 
   if(argc <= 1){
     wc(0, "");
-    exit();
+    exit(EXIT_SUCCESS);
   }
 
   for(i = 1; i < argc; i++){
     if((fd = open(argv[i], 0)) < 0){
       printf(1, "wc: cannot open %s\n", argv[i]);
-      exit();
+      exit(EXIT_FAILURE);
     }
     wc(fd, argv[i]);
     close(fd);
   }
-  exit();
+  exit(EXIT_SUCCESS);
 }
