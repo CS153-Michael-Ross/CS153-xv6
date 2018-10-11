6055c514368fbdbeac8f37ecf5ba38825cce8adb
Michael Bradley <mbrad012@ucr.edu>
2018-10-10 20:15:21 -0700

    Fix sys_exit call

--- sysproc.c
+++ sysproc.c
@@ -13,10 +13,11 @@ sys_fork(void)
   return fork();
 }
 
+//Should this take a parameter?
 int
-sys_exit(int status)
+sys_exit(void)
 {
-  exit(status);
+  exit(0);
   return 0;  // not reached
 }
 
